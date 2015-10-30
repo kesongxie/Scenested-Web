ae8ab013afbbf33e80f00eccf936054fdcd18547
Kesong Xie <kesongxie1993@gmail.com>
2015-10-30 16:39:30 -0500

    updated

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 	session_start();
 	include_once 'php_inc/global_constant.php';
 	include_once MODEL_PATH.'Auth_Tokens.php';
-	
+
 	$auth_tokens = new Auth_Tokens();
 	if(isset($_SESSION['id']) || $auth_tokens->auth_token_valified()){
 		header('location:'.HOME_PAGE);
