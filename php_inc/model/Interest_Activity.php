6dbf9a82e209a7afc98e799e1a8ad65a859f80fd
Kesong Xie <kesongxie1993@gmail.com>
2015-10-30 17:34:33 -0400

    disable display activation message

--- php_inc/model/Interest_Activity.php
+++ php_inc/model/Interest_Activity.php
@@ -207,7 +207,6 @@
 			$user = new User_Table();
 			include_once 'Education.php';
 			$educ = new Education();
-			$activated = $user->isUserActivated($_SESSION['id']);
 			$email = $user->getUserIden($_SESSION['id']);
 			$profile_pic = $profile->getLatestProfileImageForUser($_SESSION['id']);
 			$cover_pic =  $user->getLatestCoverForuser($_SESSION['id']);
