e27256a573ae4c427aaf8cd3511629d6b540945a
Kesong Xie <kesongxie1993@gmail.com>
2015-12-30 12:13:37 -0500

    generally finish two column post layout

--- js/core.js
+++ js/core.js
@@ -1,3 +1,16 @@
+/*************** starts global variable ***********/
+var mouseX = 0;
+var mouseY = 0;
+var mouseViewPortX = 0;
+var mouseViewPortY = 0;
+var TWO_COLUMN_HORIZON_ANGLE_SLOPE = 5;
+var TWO_COLUMN_VERTICAL_ANGLE_SLOPE = 0.4;
+
+var LAYOUT_MODE = {'twoColumnHorizon':'two-column-horizon','twoColumnVertical':'two-column-vertical'}
+
+/*************** ends global variable ***********/
+
+
 /*starts scene box editor functions*/
 function replacer(match, p1, p2, p3, p4){
  	var re = /^\s$/;
@@ -191,13 +204,35 @@ function readURL(input,tg) {
 }
 
 
-/*************** starts global variable ***********/
-var mouseX = 0;
-var mouseY = 0;
-var ANGLE_SLOPE = 5;
 
-/*************** ends global variable ***********/
+function resetTwoColumnLayout(){
+	var containers = $('#layout-draggable').find('.photo-segment-container');
+	containers.removeClass('two-column-vertical').addClass('two-column-horizon');
+	setLayoutMode(LAYOUT_MODE.twoColumnHorizon);
+	containers.find('.photo-segment').css({'left':'0px', 'top':'0px', 'right':'0px', 'bottom':'0px'});
+	containers.find('.photo').attr('src','').css('margin', '0px');
+}
 
+
+function setLayoutMode(mode){
+	$('#layout-draggable').attr('data-layout-mode',mode); 
+}
+
+function getLayoutMode(){
+	return $('#layout-draggable').attr('data-layout-mode'); 
+}
+
+function isPostPhotoModified(){
+	return $('#attach-post-photo-banner').attr('data-modified') == 'true';
+}
+
+function setPostPhotoModified(){
+	$('#attach-post-photo-banner').attr('data-modified','true');
+}
+
+function unsetPostPhotoModified(){
+	$('#attach-post-photo-banner').attr('data-modified','false');
+}
 
 $(document).ready(function(){
 	// $('button input div img').on('click', function(e){
@@ -368,6 +403,8 @@ $(document).ready(function(){
 						$(thisE).val('');
 					}
 			});
+			setPostPhotoModified();
+			
 		}
 	},'#attach-post-photo');
 	
@@ -388,9 +425,11 @@ $(document).ready(function(){
 					}
 				}
 			});
+			
+			setPostPhotoModified();	
+			return false;
 		}
 	},'.post-photo-thumbnail .remove-circle-icon');
-	
 	
 // 	 $( "#layout-draggable" ).sortable({
 //      	 revert: true
@@ -399,78 +438,276 @@ $(document).ready(function(){
 	var startDragX = 0, startDragY = 0;
 	var stopDragX = 0, stopDragY = 0;
 	
-	$('.photo-segment').draggable({
-		containment:'#layout-draggable',
+	var initial_rect;
+	
+	/*$('.photo').draggable({
+		containment:'#photo-constriant',
 		axis:'y',
 		scroll:false,
-		revert:"invalid",
-		start:function(){
+		revert:'valid',
+		opacity:0.8,
+		zIndex:100,
+		start:function(event,ui){
 			startDragX = mouseX;
 			startDragY = mouseY;
+			initial_rect = ui.helper.parents('.photo-segment-container')[0].getBoundingClientRect();
 		},
 		drag:function(event,ui){
-			$(this).css({'z-index':9999, 'opacity':'0.8'});
+			
+			if(initial_rect.left < mouseViewPortX && mouseViewPortX < initial_rect.right && initial_rect.top < mouseViewPortY && mouseViewPortY < initial_rect.bottom){
+				console.log('in');
+				//$(this).draggable( "option", "containment", "#photo-constriant" );
+// 				$(this).draggable( "option", "revert", "false" );
+			}else{	
+				console.log('out');
+				$(this).draggable( "option", "containment", "" );
+// 				$(this).draggable( "option", "revert", "invalid" );
+			}
 		},
 		stop:function(event,ui){
-			$(this).css({'z-index':'', 'opacity':'1'});
+			
 		}
 	});
-	
-	// $('.photo').draggable({
-// 		containment:'parent',
-// 		scroll:false,
-// 		drag:function(event,ui){
-// 		
-// 		// 	ui.helper.addClass('drop-photo-scale-animation');
-// 		},
-// 		stop:function(event,ui){
-// 			// ui.helper.removeClass('drop-photo-scale-animation');
-// 
-// 		}
-// 	});
-// 	
+*/
+
 	
 	var drag_left = false;
 	var drag_down = false;
 	var swap = false;
 	
-	$('.photo-segment-container').droppable({
-		accept:'.photo-segment',
-		tolerance:'intersect',
+ 	/*$('.photo-segment-container').droppable({
+		accept:'.photo',
+		tolerance:'pointer',
 		over:function(event, ui){
+			var mode = getLayoutMode();
 			var des_container = $(this);
-			var des_container = $(this);
-			var src_target = ui.draggable;
+			var src_target = ui.draggable; //photo
 			var des_container_queue = des_container.attr('data-container-queue');
-			var src_target_queue = src_target.attr('data-segment-queue');
+			var src_target_queue = src_target.parent('.photo-segment').attr('data-segment-queue');
 			if(des_container_queue != src_target_queue){
 				des_container.find('.photo').addClass('drop-photo-scale-animation');
 				var title_text = $('#post-photo-layout-segue .dialog-title-text');
 				stopDragX = mouseX;
 				stopDragY = mouseY;
-				
-				
-				var slope = (stopDragY - startDragY)/(stopDragX - startDragX);
-				if( -ANGLE_SLOPE < slope && slope < ANGLE_SLOPE ){
-					drag_down = (stopDragY - startDragY > 0);
-					drag_left = (drag_down) ? (slope > 0?false:true) : (slope > 0?true:false);
-					if(drag_left){
-						title_text.text('Drag to left');	
-
+				if(mode == LAYOUT_MODE.twoColumnHorizon){
+					var slope = (stopDragY - startDragY)/(stopDragX - startDragX);
+					if( -TWO_COLUMN_HORIZON_ANGLE_SLOPE < slope && slope < TWO_COLUMN_HORIZON_ANGLE_SLOPE ){
+						drag_down = (stopDragY - startDragY > 0);
+						drag_left = (drag_down) ? (slope > 0?false:true) : (slope > 0?true:false);
+						if(drag_left){
+							title_text.text('Drag to left');	
+						}else{
+							title_text.text('Drag to right');	
+						}
+						swap = false;
 					}else{
-						title_text.text('Drag to right');	
+						title_text.text('Swap');
+						swap = true;
 					}
-					swap = false;
-				}else{
-					title_text.text('Swap');
-					swap = true;
+				}else if(mode == LAYOUT_MODE.twoColumnVertical){
+					var slope = (stopDragY - startDragY)/(stopDragX - startDragX);
+					if( -TWO_COLUMN_VERTICAL_ANGLE_SLOPE < slope && slope < TWO_COLUMN_VERTICAL_ANGLE_SLOPE){
+						title_text.text('Swap');
+						swap = true;
+					}else{
+						drag_down = (stopDragY - startDragY > 0);
+						drag_left = (drag_down) ? (slope > 0?false:true) : (slope > 0?true:false);
+						if(drag_down){
+							title_text.text('Drag to bottom');	
+						}else{
+							title_text.text('Drag to top');	
+						}
+						swap = false;	
+					}
+				}
+			}
+		},
+		drop:function(event, ui){
+		 	var des_container = $(this); //container
+			var src_target = ui.draggable; //photo
+			
+			var des_target = des_container.find('.photo'); //photo
+			var src_container = src_target.parents('.photo-segment-container'); //container
+			
+			var des_container_queue = des_container.attr('data-container-queue');
+			var src_target_queue = src_target.parent('.photo-segment').attr('data-segment-queue');
+			
+			if(des_container_queue != src_target_queue){
+				//swap the element 
+				var changeToVertical = (!drag_down && drag_left )  || (drag_down && !drag_left) ;
+				if(swap ||  changeToVertical){
+					var src_url = src_target.attr('src');
+					var des_url = des_target.attr('src');
+					src_target.attr('src',des_url);
+					des_target.attr('src', src_url);
+				}
+				if(!swap){
+					if(getLayoutMode() == LAYOUT_MODE.twoColumnHorizon){
+						src_container.removeClass('two-column-horizon').addClass('two-column-vertical');
+						des_container.removeClass('two-column-horizon').addClass('two-column-vertical');
+						src_target.draggable('option','axis','x');
+						des_target.draggable('option','axis','x');
+						setLayoutMode(LAYOUT_MODE.twoColumnVertical);
+					}else{
+						src_container.removeClass('two-column-vertical').addClass('two-column-horizon');
+						des_container.removeClass('two-column-vertical').addClass('two-column-horizon');
+						src_target.draggable('option','axis','y');
+						des_target.draggable('option','axis','y');
+						setLayoutMode(LAYOUT_MODE.twoColumnHorizon);
+					}
+					
 				}
 			}
 			
-			
+				$('#layout-draggable .photo-segment .photo').css({'left':'0px', 'top':'0px'});
 		
+			$('.photo-segment-container .photo').removeClass('drop-photo-scale-animation');
+			var title_text = $('#post-photo-layout-segue .dialog-title-text');
+			title_text.text($(title_text).attr('data-default-title'));
+			
+		}, 
+		deactivate:function(event,ui){
+			return false;
+		}
+	
+	});
+	*/
+	
+	
+	
+	
+	
+	
+	/* --------droppable working, but unadjustable---------- */
+	
+
+	$('.photo-segment').draggable({
+		containment:'#layout-draggable',
+		scroll:false,
+		revert:"invalid",
+		opacity:0.8,
+		zIndex:100,
+		helper:function(){
+			return '<div></div>';	
+		},
+		start:function(event, ui){
+			startDragX = mouseX;
+			startDragY = mouseY;
+			initial_rect = $(this).parents('.photo-segment-container')[0].getBoundingClientRect();
+		},
+		drag:function(event,ui){
+			if(initial_rect.left < mouseViewPortX && mouseViewPortX < initial_rect.right && initial_rect.top < mouseViewPortY && mouseViewPortY < initial_rect.bottom){
+				//don't move the element at all, using the default helper that make the element looks like static
+				
+				var photo = $(this).find('.photo');
+				var mode = getLayoutMode();
+				if(mode == LAYOUT_MODE.twoColumnHorizon){
+					var max_dropppable_distance = photo.height() - 256;
+					if(mouseY - startDragY > 0){
+						//down
+						var current_margin_top = parseInt(photo.css('margin-top'));
+						if(current_margin_top < 0){
+							current_margin_top +=4;
+							current_margin_top = (current_margin_top > 0)?0:current_margin_top;
+							photo.animate({'margin-top': current_margin_top+'px'}, 0);
+						}
+					}else{
+						var current_margin_top = parseInt(photo.css('margin-top'));
+						if( current_margin_top > -max_dropppable_distance  ){
+							current_margin_top -=4;
+							current_margin_top = (current_margin_top < -max_dropppable_distance)?-max_dropppable_distance:current_margin_top;
+							photo.animate({'margin-top': current_margin_top+'px'}, 0);
+						}	
+					}
+				}else if(mode == LAYOUT_MODE.twoColumnVertical){
+					var max_dropppable_distance = photo.width() - 256;
+					if(mouseX - startDragX > 0){
+						//down
+						var current_margin_top = parseInt(photo.css('margin-left'));
+						if(current_margin_top < 0){
+							current_margin_top +=4;
+							current_margin_top = (current_margin_top > 0)?0:current_margin_top;
+							photo.animate({'margin-left': current_margin_top+'px'}, 0);
+						}
+					}else{
+						var current_margin_top = parseInt(photo.css('margin-left'));
+						if( current_margin_top > -max_dropppable_distance  ){
+							current_margin_top -=4;
+							current_margin_top = (current_margin_top < -max_dropppable_distance)?-max_dropppable_distance:current_margin_top;
+							photo.animate({'margin-left': current_margin_top+'px'}, 0);
+						}	
+					}
+				}
+				
+				
+				
+				
+				
+				
+					
+			}else{	
+				//when the cursor move out of the original container, make the helper becomes original that make it look it's moving
+				
+				//$(this).addClass('hdn');
+				$(this).draggable( "option", "helper", "original" );
+			}
+		},
+		stop:function(event,ui){
+					
+		}
+	});
+	
+	
+	$('.photo-segment-container').droppable({
+		accept:'.photo-segment',
+		tolerance:'pointer',
+		over:function(event, ui){
+			var mode = getLayoutMode();
+			var des_container = $(this);
+			var des_container = $(this);
+			var src_target = ui.draggable;
+			var des_container_queue = des_container.attr('data-container-queue');
+			var src_target_queue = src_target.attr('data-segment-queue');
 			
 			
+			console.log(des_container_queue+" "+src_target_queue );
+			if(des_container_queue != src_target_queue){
+				des_container.find('.photo').addClass('drop-photo-scale-animation');
+				var title_text = $('#post-photo-layout-segue .dialog-title-text');
+				stopDragX = mouseX;
+				stopDragY = mouseY;
+				if(mode == LAYOUT_MODE.twoColumnHorizon){
+					var slope = (stopDragY - startDragY)/(stopDragX - startDragX);
+					if( -TWO_COLUMN_HORIZON_ANGLE_SLOPE < slope && slope < TWO_COLUMN_HORIZON_ANGLE_SLOPE ){
+						drag_down = (stopDragY - startDragY > 0);
+						drag_left = (drag_down) ? (slope > 0?false:true) : (slope > 0?true:false);
+						if(drag_left){
+							title_text.text('Drag to left');	
+						}else{
+							title_text.text('Drag to right');	
+						}
+						swap = false;
+					}else{
+						title_text.text('Swap');
+						swap = true;
+					}
+				}else if(mode == LAYOUT_MODE.twoColumnVertical){
+					var slope = (stopDragY - startDragY)/(stopDragX - startDragX);
+					if( -TWO_COLUMN_VERTICAL_ANGLE_SLOPE < slope && slope < TWO_COLUMN_VERTICAL_ANGLE_SLOPE){
+						title_text.text('Swap');
+						swap = true;
+					}else{
+						drag_down = (stopDragY - startDragY > 0);
+						drag_left = (drag_down) ? (slope > 0?false:true) : (slope > 0?true:false);
+						if(drag_down){
+							title_text.text('Drag to bottom');	
+						}else{
+							title_text.text('Drag to top');	
+						}
+						swap = false;	
+					}
+				}
+			}
 		},
 		drop:function(event, ui){
 			var des_container = $(this);
@@ -490,10 +727,28 @@ $(document).ready(function(){
 					des_container.find('.photo-segment').html(src_content);
 				}
 				
+				var currentMode = getLayoutMode();
 				if(!swap){
-					src_container.removeClass('two-column-horizon').addClass('two-column-vertical');
-					des_container.removeClass('two-column-horizon').addClass('two-column-vertical');
+					if( currentMode == LAYOUT_MODE.twoColumnHorizon){
+						src_container.removeClass('two-column-horizon').addClass('two-column-vertical');
+						des_container.removeClass('two-column-horizon').addClass('two-column-vertical');
+						src_target.draggable('option','axis','x');
+						des_target.draggable('option','axis','x');
+						
+						
+						setLayoutMode(LAYOUT_MODE.twoColumnVertical);
+					}else{
+						src_container.removeClass('two-column-vertical').addClass('two-column-horizon');
+						des_container.removeClass('two-column-vertical').addClass('two-column-horizon');
+						src_target.draggable('option','axis','y');
+						des_target.draggable('option','axis','y');
+						
+						setLayoutMode(LAYOUT_MODE.twoColumnHorizon);
+					}
 				}
+				var photo_segment = $('#layout-draggable .photo-segment');
+				photo_segment.css({'left':'0px', 'top':'0px', 'right':'0px', 'bottom':'0px'});
+				photo_segment.find('.photo').css('margin', '0px');
 				
 			}
 			$('#layout-draggable .photo-segment').css({'top':'0px'});
@@ -508,7 +763,6 @@ $(document).ready(function(){
 	
 	});
 	
-	
 	$('body #edit-dialog-wrapper-inner').on({
 		click:function(){
 			var banner = $(this).parents('#attach-post-photo-banner');
@@ -516,15 +770,21 @@ $(document).ready(function(){
 			if(length > 1){
 				toggleDialogVerticalVerticalCenterPos();
 				var segue_wrapper = $(this).parents('.segue-wrapper');
+				segue_wrapper.css('position','relative');
 				var wrapper_width = segue_wrapper.width();
 				var segue_main = segue_wrapper.find('.segue-main').removeClass('act');
 				var post_layout_segue = $('#post-photo-layout-segue');
 				var layout_body = post_layout_segue.find('.photo-layout-body');
-				banner.find('.photo-thumbnail').each(function(index){
-					var container = layout_body.find('.photo-segment-container[data-container-queue="'+(index+1)+'"]');
-					container.find('.photo').attr('src', $(this).attr('src'));
-					container.removeClass('hdn');
-				});
+				if(isPostPhotoModified()){
+					resetTwoColumnLayout();
+					banner.find('.photo-thumbnail').each(function(index){
+						var container = layout_body.find('.photo-segment-container[data-container-queue="'+(index+1)+'"]');
+						container.removeClass('hdn');
+						var photo = container.find('.photo').attr('src', $(this).attr('src'));
+					});
+					unsetPostPhotoModified();
+				}
+			
 				segue_wrapper.css('height',segue_main.height());
 				segue_main.addClass('hdn');
 				post_layout_segue.css({'-webkit-animation':'segueSlideInLeft 0.3s','animation':'segueSlideInLeft 0.3s', 'right':'0px'}).addClass('act');
@@ -538,11 +798,8 @@ $(document).ready(function(){
 				segue_wrapper.css('position','relative');
 			 
 			 }
-			 
-			 
 		}
 	},'.post-photo-thumbnail');
-	
 	
 	
 	
@@ -557,13 +814,16 @@ $(document).keyup(function(e){
 });
 
 $(document).click(function(){
-		closeEditDialog(false);
+		//closeEditDialog(false);
 });
 
 
 $(document).on('mousemove',function(e){
 		mouseX = e.pageX;
 		mouseY = e.pageY;
+		mouseViewPortX = e.clientX;
+		mouseViewPortY = e.clientY;
+		
 });
 
 	
